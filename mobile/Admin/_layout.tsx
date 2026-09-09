91305f40c0ca267502b70ed45729ebc8182b27ff
ahmadbaim459 <ahmadbaim459@gmail.com>
2026-09-09 10:57:14 +0700

    Delete mobile/Admin directory

--- mobile/Admin/_layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Tabs } from "expo-router";
-
-export default function TabLayout() {
-  return (
-    <Tabs screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }}>
-      <Tabs.Screen name="index" options={{ title: "MEDSTAFF" }} />
-    </Tabs>
-  );
-}
