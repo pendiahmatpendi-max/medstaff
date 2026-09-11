7f4ad343544078f0ead6b49d338567c6d8ad2917
Pendi Ahmat <pendiahmatpendi@gmail.com>
2026-09-10 23:40:57 -0700

    Fix activity auth dependency on Vercel

--- backend/src/activity/activity.module.ts
+++ backend/src/activity/activity.module.ts
@@ -4,11 +4,13 @@ import { ActivityController } from './activity.controller';
 import { ActivityService } from './activity.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { NotificationModule } from '../notification/notification.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     PrismaModule,
     NotificationModule,
+    AuthModule,
   ],
   controllers: [ActivityController],
   providers: [ActivityService],
